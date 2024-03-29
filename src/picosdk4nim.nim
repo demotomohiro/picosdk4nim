6dad43e16d1343fda4f65e935174e58884b77f29
demotomohiro <gpuppur@gmail.com>
2024-03-30 05:15:09 +0900

    Can change UART instance and pin used for stdio

--- src/picosdk4nim.nim
+++ src/picosdk4nim.nim
@@ -20,6 +20,20 @@ when defined(PicoEnableStdioUart):
     ## Enable reading from stdout and writing to stdin through UART.
     ## Enabled in default.
 
+when defined(PicoDefaultUart):
+  const PicoDefaultUart {.intdefine.} = 0
+    ## Set UART instance for stdio.
+    ## Must be 0 or 1
+  static: assert PicoDefaultUart in 0 .. 1
+when defined(PicoDefaultUartTxPin):
+  const PicoDefaultUartTxPin {.intdefine.} = 0
+    ## Set UART Tx GPIO pin number used as stdout.
+  static: assert PicoDefaultUartTxPin mod 4 == 0 and PicoDefaultUartTxPin in 0 .. 28
+when defined(PicoDefaultUartRxPin):
+  const PicoDefaultUartRxPin {.intdefine.} = 1
+    ## Set UART Rx GPIO pin number used as stdin.
+  static: assert PicoDefaultUartRxPin mod 4 == 1 and PicoDefaultUartRxPin in 1 .. 29
+
 const cmakeStmts = block:
   var res = @[initCMakeInclude($(PicoSDKPath.PathX[:fdDire, arAbso, BuildOS, true].joinFile"pico_sdk_init.cmake"), "includePicoSDK"),
               initCMakeCmd("pico_sdk_init()", "initPicoSDK", "std.project")]
@@ -35,6 +49,13 @@ const cmakeStmts = block:
     res.add initCMakeCmdWithTarget(fmt"pico_enable_stdio_usb(#target {ord(PicoEnableStdioUsb)})")
   when defined(PicoEnableStdioUart):
     res.add initCMakeCmdWithTarget(fmt"pico_enable_stdio_uart(#target {ord(PicoEnableStdioUart)})")
+
+  when defined(PicoDefaultUart):
+    res.add initCMakeCmdWithTarget(fmt"target_compile_definitions(#target PRIVATE PICO_DEFAULT_UART={PicoDefaultUart})")
+  when defined(PicoDefaultUartTxPin):
+    res.add initCMakeCmdWithTarget(fmt"target_compile_definitions(#target PRIVATE PICO_DEFAULT_UART_TX_PIN={PicoDefaultUartTxPin})")
+  when defined(PicoDefaultUartRxPin):
+    res.add initCMakeCmdWithTarget(fmt"target_compile_definitions(#target PRIVATE PICO_DEFAULT_UART_RX_PIN={PicoDefaultUartRxPin})")
 
   res
 
